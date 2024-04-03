f09b4a703b89cd0fbc3eee6ba65fefbb00ba3ff5
Isaac1502 <isaactadinagiraldo02@gmail.com>
2024-04-02 21:20:13 -0500

    utilities added

--- Proyecto1-sospina-itadina/nameserver/nameserver.py
+++ Proyecto1-sospina-itadina/nameserver/nameserver.py
@@ -13,6 +13,56 @@ import rpyc
 from threading import Thread
 
 
+def run_cmd_storage_servers(formatted_cmd_str, storage_servers):
+    for ss in storage_servers:
+        ss = ss.split(":")
+        try:
+            conn = rpyc.connect(ss[0], ss[2])
+            conn.root.run_shell_cmd(formatted_cmd_str)
+            print('Run "{}" in {}'.format(formatted_cmd_str, ss[0] + ":" + ss[2]))
+        except ConnectionRefusedError:
+            print(
+                'Error while trying to connect to {} to run cmd "{}"'.format(
+                    ss, formatted_cmd_str
+                )
+            )
+
+
+def ask_ss_to_forward_blocks(source, destinations, blocks):
+    print("Asking {} to distribute {} to ss {}".format(source, blocks, destinations))
+    src = src.split(":")
+    try:
+        conn = rpyc.connect(source[0], source[2])
+        conn.root.forward_my_blocks(blocks, destinations)
+    except ConnectionRefusedError:
+        print(
+            "Connection refused by {} while trying to ask to forward blocks.".format(
+                source
+            )
+        )
+
+
+def send_directory_structure_to_sync(target, structure):
+    ss = target.split(":")
+    try:
+        conn = rpyc.connect(ss[0], ss[2])
+        conn.root.adapt_this_directory_structure(json.dumps(structure))
+        print("Send directory structure to {} for syncing".format(target))
+    except ConnectionRefusedError:
+        print(
+            "Connection refused by {} while trying to send dir structure".format(target)
+        )
+
+
+def this_ss_is_alive(ip, port):
+    try:
+        conn = rpyc.connect(ip, int(port))
+        conn.close()
+        return True
+    except ConnectionRefusedError:
+        return False
+
+
 class NameServerService(rpyc.Service):
     config = None
     struct = {}
@@ -20,6 +70,17 @@ class NameServerService(rpyc.Service):
     ABSOLUTE_ROOT = None  # root directory in storage servers /ken
     STRUCT_ROOT = None
     searched_files = []
+
+    def __init__(self):
+        self.exposed_refresh()
+        print("Name server started...")
+        thread = Thread(target=self.check_aliveness, args=(0,), daemon=True)
+
+    def exposed_refresh(self):
+        pass
+
+    def check_aliveness(self):
+        pass
 
 
 if __name__ == "__main__":
