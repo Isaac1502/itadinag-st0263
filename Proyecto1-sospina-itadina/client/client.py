d52bc5142c12e174a6a067c34af55c99d9a5223a
Isaac1502 <isaactadinagiraldo02@gmail.com>
2024-04-06 22:33:30 -0500

    client prompt class initial approach

--- Proyecto1-sospina-itadina/client/client.py
+++ Proyecto1-sospina-itadina/client/client.py
@@ -109,3 +109,73 @@ def get_from_ss(ss, remote_path):
         return conn.root.get(remote_path)
     except ConnectionRefusedError:
         print("Connection refused by {} while trying to get {}".format(ss, remote_path))
+
+
+def parse_size_from_bytes(num):
+    num = float(num)
+    # this function will convert bytes to MB.... GB... etc
+    for x in ["bytes", "KB", "MB", "GB", "TB"]:
+        if num < 1024.0:
+            return "%3.2f %s" % (num, x)
+        num /= 1024.0
+
+
+def prettyDictionary(t, s):
+    for key in sorted(t.keys()):
+        if "blocks" in t[key]:
+            print(HTML("   " * (s) + "|- " + str(key)))
+        else:
+            key_str = "root" if key == "ken" else key
+            if key_str == "root":
+                print(HTML(" <b>ROOT</b>"))
+            else:
+                print(HTML("   " * s + "|- <b><green>" + str(key_str) + "</green></b>"))
+
+            if not isinstance(t[key], list):
+                prettyDictionary(t[key], s + 1)
+
+
+from cmd import Cmd
+
+
+class MyPrompt(Cmd):
+    global CONN
+    ABSOLUTE_ROOT = "/ken"
+    CURRENT_DIR = "/ken"
+    prompt = "[KEN-DFS] (.) >> "
+    intro = "Welcome to KEN Distributed File System.\n > Type '?' or 'help' to see available commands.\n > The root volume is /ken/ \n"
+
+    def preloop(self):
+        print(HTML("\n------------- <green>Session Started</green> -------------\n"))
+
+    def parse_args(self, cmd_name, args, min_required=0, max_required=float("inf")):
+        args = args.strip()
+        args = args.split(" ") if len(args) > 0 else args
+        N = len(args)
+
+        if N < min_required or N > max_required:
+            if min_required == max_required:
+                required = min_required
+            else:
+                required = "{} ".format(min_required)
+                if math.isinf(max_required):
+                    required += "or more"
+                else:
+                    required += "to {}".format(max_required)
+
+            print(
+                HTML(
+                    "<red>Error</red>: <b>{}</b> expected <orange>{}</orange> args, got <orange>{}</orange>.".format(
+                        cmd_name, required, N
+                    )
+                )
+            )
+            print(
+                HTML(
+                    "<green><b>TIP</b></green>: Try <b>help {}</b> for correct usage.\n".format(
+                        cmd_name
+                    )
+                )
+            )
+            return None
+        return args
